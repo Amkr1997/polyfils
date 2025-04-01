7afffd0263283687fd6eb8b37f095d2333406483
Aman Khare <khareaman754@gmail.com>
2025-04-01 12:49:12 +0530

    feat: created reducePolyfils.js

---
+++ reducePolyfils.js
@@ -0,0 +1,25 @@
+const arr = [1, 2, 3, 4, 5, 6, 2, 3, 4];
+
+Array.prototype.myReduce = function (cb, initialValue) {
+  let acc = initialValue;
+  let initialIndex = 0;
+
+  if (arguments.length < 2) {
+    acc = this[0];
+    initialIndex = 1;
+  }
+
+  for (let i = 0; i < this.length; i++) {
+    acc = cb(acc, this[i], i, this);
+  }
+
+  return acc;
+};
+
+const result = arr.myReduce((acc, curr) => {
+  acc[curr] = (acc[curr] || 0) + 1;
+
+  return acc;
+}, {});
+
+console.log(result);
