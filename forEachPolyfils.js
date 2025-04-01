f3ae57a16d5315b50e41672451a629b404f7b730
Aman Khare <khareaman754@gmail.com>
2025-04-01 10:30:32 +0530

    feat: Adding forEach polyfil

---
+++ forEachPolyfils.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 4, 5, 6];
+
+function cb(el) {
+  console.log(el);
+}
+
+Array.prototype.myForEach = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    cb(this[i]);
+  }
+};
+
+arr.myForEach(cb);
